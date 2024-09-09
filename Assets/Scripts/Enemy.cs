9393809b452b0893601d88d7fddd137b758e34a0
Egor <m.fishborn@yandex.ru>
2024-09-09 18:26:17 +0300

    init

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,12 +5,15 @@ public class Enemy : MonoBehaviour
     [SerializeField] private Waypoint _pointA;
     [SerializeField] private Waypoint _pointB;
     [SerializeField] private float _speed = 1f;
+    [SerializeField] private Player _player;
 
     public readonly int isRunning = Animator.StringToHash(nameof(isRunning));
     public Animator Animator;
     private Rigidbody2D _rigidbody;
     private Transform _currentWaypointToGo;
     private float _distanceToWaypoint = 0.5f;
+    private bool _isPlayerNear = false;
+    private Transform _playerPosition;
 
     private void Start()
     {
@@ -21,7 +24,47 @@ public class Enemy : MonoBehaviour
 
     private void Update()
     {
-        Patrol();
+        if (_isPlayerNear)
+        {
+            Chase();
+        }
+        else
+        {
+            Patrol();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.TryGetComponent(out Player player))
+        {
+            _isPlayerNear = true;
+            _playerPosition = player.transform;
+        }
+        else
+        {
+            _isPlayerNear = false;
+        }
+    }
+
+    private void Attack()
+    {
+
+    }
+
+    private void Chase()
+    {
+        float directionChanger = -1f;
+        Vector2 point = _playerPosition.position - transform.position;
+
+        if (_playerPosition.position.x > transform.position.x)
+        {
+            _rigidbody.velocity = new Vector2(_speed , 0);
+        }
+        else
+        {
+            _rigidbody.velocity = new Vector2(_speed * directionChanger, 0);
+        }
     }
 
     private void Patrol()
@@ -40,15 +83,14 @@ public class Enemy : MonoBehaviour
 
         if ((transform.position - _currentWaypointToGo.position).sqrMagnitude < _distanceToWaypoint * _distanceToWaypoint)
         {
-            FlipSide();
+            FlipSide(directionChanger);
             _currentWaypointToGo = _currentWaypointToGo == _pointB.transform ? _pointA.transform : _pointB.transform;
         }
     }
 
-    private void FlipSide()
+    private void FlipSide(float sideChanger)
     {
-        float sideChanger = -1f;
-
+        
         Vector3 localScale = transform.localScale;
         localScale.x *= sideChanger;
         transform.localScale = localScale;
